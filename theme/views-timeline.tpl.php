c4db2aa3a037a2fe73a8828092e1cf3f06b41dfa
aczietlow <chris.zietlow@morris.com>
2013-05-31 13:43:42 -0400

    applied result array structure to tpl layout

--- theme/views-timeline.tpl.php
+++ theme/views-timeline.tpl.php
@@ -14,149 +14,25 @@
 				
 				<div class="timelineWrapper">
 					<ol class="timelineSlider">
+						<?php foreach($result as $key => $event) :?>
 						<li class="eventSlide" id="year1785">
 							<div class="image">
-								<img src="i/chronicle-1785.jpg" alt="" />
+								<img src="<?php print $event['image']['uri']; ?>" alt="<?php print $event['image']['alt']; ?>" />
 								<p class="caption">
-									Photo by Luke Skywalker
+									<?php print $event['image']['caption']; ?>
 								</p>
 							</div>
 							<div class="text">
-								<h6 class="date">1785</h6>
-								<h5 class="title">Augusta Chronicle Founded</h5>
+								<h6 class="date"><?php print $event['date']; ?></h6>
+								<h5 class="title"><?php print $event['title']; ?></h5>
 								<p class="desc">
-									The Augusta Chronicle was started as a weekly newspaper called the Augusta Gazette,
-									taking the name Augusta Chronicle and Gazette of the State four years later. It is the
-									third oldest continuously published newspaper in the United States, after the New
-									Hampshire Gazette (1756) and the Hartford Courant (1764). It was the first media
-									property owned by the Morris family and remains the heart and soul of the Morris
-									Communications company.
+									<?php print $event['desc']; ?>
 								</p>
 							</div>
 						</li>
-						<li class="eventSlide" id="year1920">
-							<div class="image">
-								<img src="i/footprint.jpg" alt="" />
-								<p class="caption">
-									Photo by Luke Skywalker
-								</p>
-							</div>
-							<div class="text">
-								<h6 class="date">1924</h6>
-								<h5 class="title">Footprint Travel Guides Founded</h5>
-								<p class="desc">
-									<strong>Footprint Travel Guides Founded in London, UK, as The South American Guidebook</strong> &#0151;
-									The passion for travel and discovery at Footprint has been reflected in guidebooks dating back to the
-									first edition of the South American Handbook in 1924. This book is the longest established travel guide
-									in the English language market. <!--Written for independent travellers looking to get off the beaten track
-									and escape the tourist crowd, World News says, "Footprint can be depended on for accurate travel
-									information and for imparting a deep sense of respect for the lands and people they cover."-->
-								</p>
-							</div>
-						</li>
-						<li class="eventSlide" id="year1930">
-							<div class="image">
-								<img src="i/sportsman.jpg" alt="" />
-								<p class="caption">
-									Photo by Luke Skywalker
-								</p>
-							</div>
-							<div class="text">
-								<h6 class="date">1935</h6>
-								<h5 class="title">Alaska Sportsman</h5>
-								<p class="desc">
-									<strong>Founded, becomes Alaska Magazine in 1969</strong> &#0151; 
-								</p>
-							</div>
-						</li>
-						<li class="eventSlide">
-							<div class="image">
-								<img src="i/where.jpg" alt="" />
-								<p class="caption">
-									Photo by Luke Skywalker
-								</p>
-							</div>
-							<div class="text">
-								<h6 class="date">1936</h6>
-								<h5 class="title">Where Magazine</h5>
-								<p class="desc">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat massa vel augue placerat id
-									porttitor lorem posuere. Nulla facilisi. Integer aliquam nisl vitae risus vehicula eget fringilla lorem
-									facilisis. Nullam eu porttitor elit.
-								</p>
-							</div>
-						</li>
-						<li class="eventSlide">
-							<div class="image">
-								<img src="i/1785.jpg" alt="" />
-								<p class="caption">
-									Photo by Luke Skywalker
-								</p>
-							</div>
-							<div class="text">
-								<h6 class="date">1936</h6>
-								<h5 class="title">Western Horseman</h5>
-								<p class="desc">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat massa vel augue placerat id
-									porttitor lorem posuere. Nulla facilisi. Integer aliquam nisl vitae risus vehicula eget fringilla lorem
-									facilisis. Nullam eu porttitor elit.
-								</p>
-							</div>
-						</li>
-						<li class="eventSlide">
-							<div class="image">
-								<img src="i/1785.jpg" alt="" />
-								<p class="caption">
-									Photo by Luke Skywalker
-								</p>
-							</div>
-							<div class="text">
-								<h6 class="date">1937</h6>
-								<h5 class="title">Guest Informant</h5>
-								<p class="desc">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat massa vel augue placerat id
-									porttitor lorem posuere. Nulla facilisi. Integer aliquam nisl vitae risus vehicula eget fringilla lorem
-									facilisis. Nullam eu porttitor elit.
-								</p>
-							</div>
-						</li>
-						<li class="eventSlide" id="year1940">
-							<div class="image">
-								<img src="i/1785.jpg" alt="" />
-								<p class="caption">
-									Photo by Luke Skywalker
-								</p>
-							</div>
-							<div class="text">
-								<h6 class="date">1947</h6>
-								<h5 class="title">The Pequot Press</h5>
-								<p class="desc">
-									<strong>Founded, becomes Globe Pequot Press in 1981</strong> &#0151; Named after a local Indian
-									tribe in Connecticut, Globe Pequot Press is a mid-sized publisher and distributor with deep roots
-									in major category publishing.  <!--An award-winning book publisher, they are unmatched in regional
-									interest and travel programs represented by imprints and series such as Insiders' Guides, TwoDot,
-									Cheap Bastards, Off the Beaten Path, Fun with the Family, and Curiosities.-->
-								</p>
-							</div>
-						</li>
-						<li class="eventSlide" id="year1950">
-							<div class="image">
-								<img src="i/morris.jpg" alt="" />
-								<p class="caption">
-									Photo by Luke Skywalker
-								</p>
-							</div>
-							<div class="text">
-								<h6 class="date">1956</h6>
-								<h5 class="title">William S. Morris III</h5>
-								<p class="desc">
-									<strong>Joins Southeastern Newspapers, the holding company formed by his father to manage
-									the growing media empire.</strong> &#0151; Billy Morris, who delivered newspapers from horseback in his
-									hometown as a boy, joined the company in 1956, a few days before his 22nd birthday, as assistant to the
-									president.
-								</p>
-							</div>
-						</li>
+						<?php endforeach;?>
+						
+						
 					</ol>
 				</div><!-- /.timelineWrapper -->
 				
