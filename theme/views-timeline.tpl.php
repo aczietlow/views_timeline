634bdb85ac4bd2d077d52d69699d533606e65247
aczietlow <chris.zietlow@morris.com>
2013-05-31 13:37:51 -0400

    added fields to results and built tpl structure

--- theme/views-timeline.tpl.php
+++ theme/views-timeline.tpl.php
@@ -1,4 +1,220 @@
-<h1>test</h1>
+<div class="debug">
 <pre>
-<?php print_r($node); ?>
+<?php dsm($node); ?>
 </pre>
+</div>
+------------
+<div class="container">
+			<div class="timelineContainer">
+				
+				<div class="timelineControls">
+					<a href="#" class="btnPrevious">&larr;</a>
+					<a href="#" class="btnNext">&rarr;</a>
+				</div><!-- /.timelineControls -->
+				
+				<div class="timelineWrapper">
+					<ol class="timelineSlider">
+						<li class="eventSlide" id="year1785">
+							<div class="image">
+								<img src="i/chronicle-1785.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1785</h6>
+								<h5 class="title">Augusta Chronicle Founded</h5>
+								<p class="desc">
+									The Augusta Chronicle was started as a weekly newspaper called the Augusta Gazette,
+									taking the name Augusta Chronicle and Gazette of the State four years later. It is the
+									third oldest continuously published newspaper in the United States, after the New
+									Hampshire Gazette (1756) and the Hartford Courant (1764). It was the first media
+									property owned by the Morris family and remains the heart and soul of the Morris
+									Communications company.
+								</p>
+							</div>
+						</li>
+						<li class="eventSlide" id="year1920">
+							<div class="image">
+								<img src="i/footprint.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1924</h6>
+								<h5 class="title">Footprint Travel Guides Founded</h5>
+								<p class="desc">
+									<strong>Footprint Travel Guides Founded in London, UK, as The South American Guidebook</strong> &#0151;
+									The passion for travel and discovery at Footprint has been reflected in guidebooks dating back to the
+									first edition of the South American Handbook in 1924. This book is the longest established travel guide
+									in the English language market. <!--Written for independent travellers looking to get off the beaten track
+									and escape the tourist crowd, World News says, "Footprint can be depended on for accurate travel
+									information and for imparting a deep sense of respect for the lands and people they cover."-->
+								</p>
+							</div>
+						</li>
+						<li class="eventSlide" id="year1930">
+							<div class="image">
+								<img src="i/sportsman.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1935</h6>
+								<h5 class="title">Alaska Sportsman</h5>
+								<p class="desc">
+									<strong>Founded, becomes Alaska Magazine in 1969</strong> &#0151; 
+								</p>
+							</div>
+						</li>
+						<li class="eventSlide">
+							<div class="image">
+								<img src="i/where.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1936</h6>
+								<h5 class="title">Where Magazine</h5>
+								<p class="desc">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat massa vel augue placerat id
+									porttitor lorem posuere. Nulla facilisi. Integer aliquam nisl vitae risus vehicula eget fringilla lorem
+									facilisis. Nullam eu porttitor elit.
+								</p>
+							</div>
+						</li>
+						<li class="eventSlide">
+							<div class="image">
+								<img src="i/1785.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1936</h6>
+								<h5 class="title">Western Horseman</h5>
+								<p class="desc">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat massa vel augue placerat id
+									porttitor lorem posuere. Nulla facilisi. Integer aliquam nisl vitae risus vehicula eget fringilla lorem
+									facilisis. Nullam eu porttitor elit.
+								</p>
+							</div>
+						</li>
+						<li class="eventSlide">
+							<div class="image">
+								<img src="i/1785.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1937</h6>
+								<h5 class="title">Guest Informant</h5>
+								<p class="desc">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat massa vel augue placerat id
+									porttitor lorem posuere. Nulla facilisi. Integer aliquam nisl vitae risus vehicula eget fringilla lorem
+									facilisis. Nullam eu porttitor elit.
+								</p>
+							</div>
+						</li>
+						<li class="eventSlide" id="year1940">
+							<div class="image">
+								<img src="i/1785.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1947</h6>
+								<h5 class="title">The Pequot Press</h5>
+								<p class="desc">
+									<strong>Founded, becomes Globe Pequot Press in 1981</strong> &#0151; Named after a local Indian
+									tribe in Connecticut, Globe Pequot Press is a mid-sized publisher and distributor with deep roots
+									in major category publishing.  <!--An award-winning book publisher, they are unmatched in regional
+									interest and travel programs represented by imprints and series such as Insiders' Guides, TwoDot,
+									Cheap Bastards, Off the Beaten Path, Fun with the Family, and Curiosities.-->
+								</p>
+							</div>
+						</li>
+						<li class="eventSlide" id="year1950">
+							<div class="image">
+								<img src="i/morris.jpg" alt="" />
+								<p class="caption">
+									Photo by Luke Skywalker
+								</p>
+							</div>
+							<div class="text">
+								<h6 class="date">1956</h6>
+								<h5 class="title">William S. Morris III</h5>
+								<p class="desc">
+									<strong>Joins Southeastern Newspapers, the holding company formed by his father to manage
+									the growing media empire.</strong> &#0151; Billy Morris, who delivered newspapers from horseback in his
+									hometown as a boy, joined the company in 1956, a few days before his 22nd birthday, as assistant to the
+									president.
+								</p>
+							</div>
+						</li>
+					</ol>
+				</div><!-- /.timelineWrapper -->
+				
+				<ol class="timelineSelector">
+					<li class="prev">&larr;</li>
+					<li><a href="#year1785">1785</a></li>
+					<li><a href="#year1920">1920</a></li>
+					<li><a href="#year1930">1930</a></li>
+					<li><a href="#year1940">1940</a></li>
+					<li><a href="#year1950">1950</a></li>
+					<li class="disabled"><a href="#year1960">1960</a></li>
+					<li class="disabled"><a href="#year1970">1970</a></li>
+					<li class="disabled"><a href="#year1980">1980</a></li>
+					<li class="disabled"><a href="#year1990">1990</a></li>
+					<li class="disabled"><a href="#year2000">2000</a></li>
+					<li class="disabled"><a href="#year2010">2010</a></li>
+					<li class="next">&rarr;</li>
+				</ol><!-- /.timelineSelector-->
+			</div><!-- /.timelineContainer -->
+		</div><!-- /.container -->
+		
+		<!--  
+		<script src="jquery.easing.1.3.js"></script>
+		<script>
+			
+			// Timeline Controls
+			$('.timelineControls a.btnPrevious').click(function () { 
+				var leftPos = $('.timelineWrapper').scrollLeft();
+				$('.timelineWrapper').animate({scrollLeft: leftPos - 200}, 800);
+			});
+			$('.timelineControls a.btnNext').click(function () { 
+				var leftPos = $('.timelineWrapper').scrollLeft();
+				$( '.timelineWrapper' ).animate({scrollLeft: leftPos + 200}, 800);
+			});
+			
+			// Timeline Slide Width
+			$('.eventSlide').click(function() {
+				$('.eventSlide.active').not(this).removeClass('active');
+				$(this).toggleClass('active');
+				$(this).toggleClass('');
+			});
+			-->
+			
+			
+			// Almost works correctly... Sigh.
+			$('.timelineSelector a').click(function () {
+				
+				//reset and highlight the clicked link
+				$('.timelineSelector a').removeClass('selected');
+				$(this).addClass('selected');
+				
+				//grab the current item
+				current = $(this);
+				
+				//scroll it to the destination
+				$('#timelineWrapper').scrollTo($(this).attr('href'), 800);
+				//cancel the link default behavior
+				return false;
+			});
+			
+		</script>
