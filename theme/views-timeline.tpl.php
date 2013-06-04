e8f6cb9ecb0755903d42c546ac76e5842b716723
aczietlow <chris.zietlow@morris.com>
2013-06-04 13:49:22 -0400

    removed static dates. with dynamically populated years.

--- theme/views-timeline.tpl.php
+++ theme/views-timeline.tpl.php
@@ -12,6 +12,7 @@ test
 				
 				<div class="timelineWrapper">
 					<ol class="timelineSlider">
+					<?php $yearNav = ''; ?>
 						<?php foreach($result as $key => $event) :?>
 						<li class="eventSlide" id="year<?php echo $event['date']; ?> ">
 							<div class="image">
@@ -29,25 +30,15 @@ test
 								</p>
 							</div>
 						</li>
+						<?php $yearNav .= "<li><a href='#year" . $event['date'] . "'>" . $event['date'] ."</a></li>\n" ;?>
 						<?php endforeach;?>
-						
 						
 					</ol>
 				</div><!-- /.timelineWrapper -->
 				
 				<ol class="timelineSelector">
 					<li class="prev">&larr;</li>
-					<li><a href="#year1785">1785</a></li>
-					<li><a href="#year1920">1920</a></li>
-					<li><a href="#year1930">1930</a></li>
-					<li><a href="#year1940">1940</a></li>
-					<li><a href="#year1950">1950</a></li>
-					<li class="disabled"><a href="#year1960">1960</a></li>
-					<li class="disabled"><a href="#year1970">1970</a></li>
-					<li class="disabled"><a href="#year1980">1980</a></li>
-					<li class="disabled"><a href="#year1990">1990</a></li>
-					<li class="disabled"><a href="#year2000">2000</a></li>
-					<li class="disabled"><a href="#year2010">2010</a></li>
+					<?php print $yearNav; ?>
 					<li class="next">&rarr;</li>
 				</ol><!-- /.timelineSelector-->
 			</div><!-- /.timelineContainer -->
